0d3f2787fe0752e89746b059fffded8ed1203b78
WeiselO <90724297+WeiselO@users.noreply.github.com>
2021-09-15 11:48:01 +0200

    Lets try this instead

---
+++ something.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Dog aDog=new Dog ("Max");
+        Dog oldDog= aDog;
+        // pass aDog to foo1
+        foo1(aDog);
+        System.out.println("The dog aDog is after foo1 is: "+ aDog.getName());
+        foo2(aDog);
+        System.out.println("The dog aDog after foo2 is: "+ aDog.getName());
+    }
+    public static void foo1(Dog d){
+        d=new Dog("Fifi");
+        System.out.println("The dog d in foo1 is: "+d.getName());
+    }
+    public static void foo2(Dog d){
+        d.setName("Fifi");
+        System.out.println("The dog d in foo2 is: "+d.getName());
+    }
+
+}
+
+class Dog{
+    String name;
+    public Dog(String name){
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
